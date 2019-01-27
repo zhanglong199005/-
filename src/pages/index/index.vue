e5356229d8b2f764b719780f93d3f8b948df44e6
zhanglong199005 <531290082@qq.com>
2019-01-27 11:55:31 +0800

    完成搜索框

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>首页</div>
+  <div class="index-container">
+    <div class="search-box">
+      <input type="text" placeholder="搜索">
+      <icon type="search" size="12"></icon>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +15,29 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+$uRed:#ff2d4a;
+.search-box{
+  background-color: $uRed;
+  padding: 20rpx 16rpx;
+  box-sizing: border-box;
+  position: fixed;
+  width: 100%;
+}
+input{
+  background-color: white;
+  width: 100%;
+  box-sizing: border-box;
+  padding-left: 376rpx;
+  display: block;
+  font-size: 24rpx;
+  height: 60rpx;
+  border-radius: 10rpx;
+}
+icon{
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+}
 </style>
